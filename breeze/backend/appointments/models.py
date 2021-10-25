a4b9af7307a3becf4e38b6b0f4d5ce012d9556bf
김채은 <chenni0531@gmail.com>
2021-10-25 23:17:19 +0900

    ✨ feat: Add appointment model
    JIRA ISSUE: S05P31A202-58

--- breeze/backend/appointments/models.py
+++ breeze/backend/appointments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Appointment(models.Model):
+    id = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='appointments')
+    datetime = models.DateTimeField()
+    middle_place = models.CharField(max_length=50)
