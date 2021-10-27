e911d7f88c2efed9a0c1f9f34f51a29dffc484bc
hyeonuk <hyeonuk27@gmail.com>
2021-10-27 14:46:55 +0900

    ✨ feat: Add my profile, breeze-image
    JIRA ISSUE: S05P31A202-71

--- breeze/frontend/src/components/home/Myprofile.vue
+++ breeze/frontend/src/components/home/Myprofile.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <img src="@/assets/breeze/logo.png" class="logo" alt="breeze-logo">
+    <div>안녕하세요 지민님</div>
+    <div>약속 요정 바라미들과 즐거운 약속을 만들러 가볼까요!</div>
     <LogoutBtn />
   </div>
 </template>
@@ -17,5 +20,7 @@ export default {
 </script>
 
 <style>
-
+.logo {
+  height: 30px;
+}
 </style>
