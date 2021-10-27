df310d5e34b427d24b1d68d7477b7f57e79c6345
hyeonuk <hyeonuk27@gmail.com>
2021-10-27 13:46:08 +0900

    ✨ feat: Add logout button component of Home
    JIRA ISSUE: S05P31A202-72

--- breeze/frontend/src/components/home/Myprofile.vue
+++ breeze/frontend/src/components/home/Myprofile.vue
@@ -1,10 +1,18 @@
 <template>
-  <div></div>
+  <div>
+    <LogoutBtn />
+  </div>
 </template>
 
 <script>
+import LogoutBtn from '@/components/home/LogoutBtn'
+
+
 export default {
   name: 'Myprofile',
+  components: {
+    LogoutBtn,
+  }
 }
 </script>
 
