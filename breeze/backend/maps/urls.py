089946876b37ac9c70166e8677a164fad5050cc7
subin <ansubin2004@gmail.com>
2021-10-27 20:43:27 +0900

    ✨ feat: Collect alcohol stores information data
    JIRA ISSUE: S05P31A202-63

--- breeze/backend/maps/urls.py
+++ breeze/backend/maps/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('middle/', views.get_middle),
     path('store/<category_id>/<filter_id>/', views.get_store),
-    path('test/', views.store_info)
+    path('test/', views.store_info),
+    path('alcohol/', views.get_alcohol),
 ]
