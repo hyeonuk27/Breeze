147662b4941c08a060f67c9609f53a28b8f1a26d
DESKTOP-KVCQHCD\multicampus <jm9521@hanmail.net>
2021-10-28 14:48:53 +0900

    ✨ feat: Add Logout
    JIRA ISSUE: S05P31A202-79

--- breeze/frontend/src/utils/request.js
+++ breeze/frontend/src/utils/request.js
@@ -27,3 +27,56 @@ export const requestLogin = async (url, headers) => {
     throw new Error(e);
   }
 };
+
+// export const requestGet = async (url, headers) => {
+//   try {
+//     const response = await axios.get(url, { headers });
+//     if (response.status === 200) {
+//       if (response.headers['jwt-access-token']) updateAccessToken(response);
+//       return response.data;
+//     }
+//     throw new Error();
+//   } catch (e) {
+
+//     throw new Error(e);
+//   }
+// };
+
+export const requestPost = async (url, data, headers) => {
+  try {
+    const response = await axios.post(url, data, { headers });
+    if (response.status === 200) {
+      if (response.headers['jwt-access-token']) setJwtTokens(response);
+      return response.data;
+    }
+    throw new Error();
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
+// export const requestPut = async (url, data, headers) => {
+//   try {
+//     const response = await axios.put(url, data, { headers });
+//     if (response.status === 200) {
+//       if (response.headers['jwt-access-token']) updateAccessToken(response);
+//       return response.data;
+//     }
+//     throw new Error();
+//   } catch (e) {
+//     throw new Error(e);
+//   }
+// };
+
+// export const requestDelete = async (url, headers) => {
+//   try {
+//     const response = await axios.delete(url, { headers });
+//     if (response.status === 200) {
+//       if (response.headers['jwt-access-token']) updateAccessToken(response);
+//       return response.data;
+//     }
+//     throw new Error();
+//   } catch (e) {
+//     throw new Error(e);
+//   }
+// };
