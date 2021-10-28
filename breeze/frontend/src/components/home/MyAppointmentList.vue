a652c75e80efe116075be1e05399d7493204a11c
hyeonuk <hyeonuk27@gmail.com>
2021-10-28 11:33:22 +0900

    ✨ feat: Add get appointment list logic
    JIRA ISSUE: S05P31A202-87

--- breeze/frontend/src/components/home/MyAppointmentList.vue
+++ breeze/frontend/src/components/home/MyAppointmentList.vue
@@ -6,11 +6,28 @@
 
 <script>
 import MyAppointment from '@/components/home/MyAppointment'
+import appointmentApi from "@/api/appointment.js";
 
 export default {
   name: 'MyAppointmentList',
   components: {
     MyAppointment,
+  },
+  data() {
+    return {
+      myAppointmentList: []
+    }
+  },
+  methos: {
+    async getMyAppointmentList() {
+      let accessToken = sessionStorage.getItem("access-token");
+      let refreshToken = sessionStorage.getItem("refresh-token");
+      let result = await appointmentApi.getMyAppointmentList({
+        "access-token": accessToken,
+        "refresh-token": refreshToken,
+      });
+      this.myAppointmentList = result;
+    }
   }
 }
 </script>
--- breeze/frontend/src/utils/request.js
+++ breeze/frontend/src/utils/request.js
@@ -8,11 +8,11 @@ const setJwtTokens = (response) => {
     sessionStorage.setItem('refresh-token', response.headers['jwt-refresh-token']);
 };
 
-// const updateAccessToken = (response) => {
-//   if (response.headers['jwt-access-token'] !== sessionStorage.getItem('access-token')) {
-//     sessionStorage.setItem('access-token', response.headers['jwt-access-token']);
-//   }
-// };
+const updateAccessToken = (response) => {
+  if (response.headers['jwt-access-token'] !== sessionStorage.getItem('access-token')) {
+    sessionStorage.setItem('access-token', response.headers['jwt-access-token']);
+  }
+};
 
 export const requestLogin = async (url, headers) => {
   try {
@@ -24,6 +24,59 @@ export const requestLogin = async (url, headers) => {
     throw new Error();
   } catch (e) {
 
+    throw new Error(e);
+  }
+};
+
+export const requestGet = async (url, headers) => {
+  try {
+    const response = await axios.get(url, { headers });
+    if (response.status === 200) {
+      if (response.headers['jwt-access-token']) updateAccessToken(response);
+      return response.data;
+    }
+    throw new Error();
+  } catch (e) {
+
+    throw new Error(e);
+  }
+};
+
+export const requestPost = async (url, data, headers) => {
+  try {
+    const response = await axios.post(url, data, { headers });
+    if (response.status === 200) {
+      if (response.headers['jwt-access-token']) setJwtTokens(response);
+      return response.data;
+    }
+    throw new Error();
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
+export const requestPut = async (url, data, headers) => {
+  try {
+    const response = await axios.put(url, data, { headers });
+    if (response.status === 200) {
+      if (response.headers['jwt-access-token']) updateAccessToken(response);
+      return response.data;
+    }
+    throw new Error();
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
+export const requestDelete = async (url, headers) => {
+  try {
+    const response = await axios.delete(url, { headers });
+    if (response.status === 200) {
+      if (response.headers['jwt-access-token']) updateAccessToken(response);
+      return response.data;
+    }
+    throw new Error();
+  } catch (e) {
     throw new Error(e);
   }
 };
