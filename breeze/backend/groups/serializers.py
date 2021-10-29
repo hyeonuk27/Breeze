693f0be08a3ce83dc45f77c40a81b9bcf57dac1f
김채은 <chenni0531@gmail.com>
2021-10-29 13:29:04 +0900

    ✨ feat: complete kakao social login logic
    JIRA ISSUE: S05P31A202-53

--- breeze/backend/groups/serializers.py
+++ breeze/backend/groups/serializers.py
@@ -1,1 +1,9 @@
 from rest_framework import serializers
+from .models import Group, Groupmember
+
+
+class GroupmemeberSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Groupmember
+        fields = '__all__'   
