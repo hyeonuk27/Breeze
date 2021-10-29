168010b8befd7ff9313a80156a07c88106d4e6c1
DESKTOP-KVCQHCD\multicampus <jm9521@hanmail.net>
2021-10-29 15:07:23 +0900

    🔨 fix: Fix URL in drf.js and auth.js
    JIRA ISSUE: S05P31A202-75

--- breeze/frontend/src/api/auth.js
+++ breeze/frontend/src/api/auth.js
@@ -1,16 +1,16 @@
 import  { requestLogin, requestPost } from '@/utils/request.js';
-import SERVER from './drf.js';
+// import SERVER from './drf.js';
 
 const authApi = {
 
   login: (data) => {
     console.log('로그인 axios 보내기 1초 전')
-    return requestLogin(`/auth/?code=${data}`)
+    return requestLogin(`/auth/login/?code=${data}`)
   },
 
   logout: () => {
     console.log('로그아웃 axios 보내기 1초 전')
-    return requestPost(SERVER.ROUTES.account + '/logout')
+    return requestPost('auth/logout')
   }
 
 };
