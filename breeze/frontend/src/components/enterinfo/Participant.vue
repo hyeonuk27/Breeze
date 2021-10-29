7f7eaa7db84e06711a1eab6c05ab337d46384cb4
정은교 <jeeg111@naver.com>
2021-10-29 16:57:55 +0900

    🎨 style: Change css of Addfriends, FindMiddleBtn components
    JIRA ISSUE: S05P31A202-95

--- breeze/frontend/src/components/enterinfo/Participant.vue
+++ breeze/frontend/src/components/enterinfo/Participant.vue
@@ -1,30 +1,42 @@
 <template>
-  <div>
-    <div
-      v-for="(part, idx) in participants"
-      :key="idx"
-      >
+  <div class="participant">
+    <div class="d-flex">
       <div class="image-box">
         <img
-          :src="require('@/assets/barami/' + part.baramiType + '.png')"
+          :src="require('@/assets/barami/' + participant.baramiType + '.png')"
           class="image-box-barami"
           alt="barami-character"
         />
-        <div>{{ part.name }}</div>
-        <div>{{ part.location }}</div>
+      </div>
+      <div class="d-flex">
+        <div class="content-box">
+          <div class="participant-name">{{ participant.name }}</div>
+          <div class="participant-location">{{ participant.location }}</div>
+        </div>
+        <button
+          class="cancel-btn"
+          @click="deleteParticipant(idx)">
+          <i class="fas fa-times"></i>
+        </button>
       </div>
     </div>
+    <hr v-if="idx < participants.length-1" class="participant-hr">
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'Participant',
-  data () {
-    return {
-    }
+  props: {
+    participant: Object,
+    idx: Number,
+  },
+  methods: {
+    ...mapActions([
+      'deleteParticipant'
+    ])
   },
   computed: {
     ...mapGetters([
@@ -34,20 +46,43 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.participant {
+  /* height: 30%; */
+}
 .image-box {
-    grid-column: 1;
-    grid-row: 2;
-    align-self: center;
-    justify-self: center;
-    height: 48px;
-    width: 48px;
+    /* grid-column: 1; */
+    /* grid-row: 2; */
+    /* align-self: left;
+    justify-self: left; */
+    /* height: 5%; */
+    margin-right: 5%;
+    margin-left: 3%;
+    width: 15%;
     padding: 7px;
     border-radius: 70%;
-    background: rgba(256, 256, 256, 0.5);
+    background: #E9EDFE;
   }
 .image-box-barami {
   width: 100%;
   height: 100%;
+}
+.content-box {
+  /* margin-left: 5%; */
+  text-align: left;
+}
+.participant-name {
+  font-weight: bold;
+}
+.participant-location {
+  font-size: 10pt;
+  color: #7B6F72;
+}
+.participant-hr {
+  color: #DDDADA;
+}
+.cancel-btn {
+  background: none;
+  border: none;
 }
 </style>
