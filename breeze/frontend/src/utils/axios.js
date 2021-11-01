d1abfc4344eb7e557cf9f8374fdb305cf173f000
DESKTOP-KVCQHCD\multicampus <jm9521@hanmail.net>
2021-11-01 15:59:18 +0900

    🔨 fix: Fix axios
    JIRA ISSUE: S05P31A202-143

--- breeze/frontend/src/utils/axios.js
+++ breeze/frontend/src/utils/axios.js
@@ -3,16 +3,21 @@ import store from "@/store"
 import SERVER from '@/api/drf.js';
 
 
-axios.defaults.baseURL = SERVER.URL
-axios.defaults.headers['Content-Type'] = 'application/json'
+const http = axios.create({
+  baseURL: SERVER.URL,
+  headers: { "content-type": "application/json" },
+})
 
-axios.interceptors.request.use(
+// axios.defaults.baseURL = SERVER.URL
+// axios.defaults.headers['Content-Type'] = 'application/json'
+
+http.interceptors.request.use(
   config => {
     const isAuthenticated = store.getters.getUserId
     console.log(isAuthenticated, '아이디가 있다면=로그인이 되었다면')
     if (isAuthenticated) {
       config.headers.common["Authorization"] = 'Bearer ' + sessionStorage.getItem("access-token");
-      console.log(config.headers.common["Authorization"], 'axios 헤더에 날라갈 액세스 토큰')
+      console.log(config.headers.common["Authorization"], 'http 헤더에 날라갈 액세스 토큰')
       config.headers.common["X-Id"] = isAuthenticated;
     }
     return config
@@ -21,6 +26,6 @@ axios.interceptors.request.use(
     Promise.reject(error)
   }
 )
-axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded"
+http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded"
 
-export default axios
+export default http
