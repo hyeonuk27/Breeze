eda59a7754c206fff80eefcb58004b93795b45a1
김채은 <chenni0531@gmail.com>
2021-11-02 16:01:41 +0900

    ✨ feat: Add logic for finding middle
    JIRA ISSUE: S05P31A202-127

--- breeze/backend/maps/views.py
+++ breeze/backend/maps/views.py
@@ -1,15 +1,44 @@
+from django.http.response import JsonResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
-from .serializers import PlaceListSerializer
+from django.contrib import auth
+
+from decouple import config
 
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+import requests
+
+
+REST_API_KEY = config('REST_API_KEY')
+User = get_user_model()
+
 
 @api_view(['POST'])
 def get_middle(request):
-    pass
+    # 중간 지점 찾기 알고리즘
+
+
+    # 중간 지점 찾고 사용자가 중간 지점으로 오는 경로 제공
+    headers = {
+        'Authorization': 'KakaoAK {REST_API_KEY}',
+    }
+
+    params = (
+        ('origin', '127.11015314141542,37.39472714688412'),
+        ('destination', '127.10824367964793,37.401937080111644'),
+        ('waypoints', ''),
+        ('priority', 'RECOMMEND'),
+        ('car_fuel', 'GASOLINE'),
+        ('car_hipass', 'false'),
+        ('alternatives', 'false'),
+        ('road_details', 'false'),
+    )
+
+    response = requests.get('https://apis-navi.kakaomobility.com/v1/directions', headers=headers, params=params)
 
 
 @api_view(['POST'])
