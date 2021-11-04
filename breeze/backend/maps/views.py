78afbd9b3aa0aa6bcb9d200af76f87809aee78d5
김채은 <chenni0531@gmail.com>
2021-11-04 11:56:39 +0900

    ✨ feat: Add logic of classifying midpoint types
    JIRA ISSUE: S05P31A202-138

--- breeze/backend/maps/views.py
+++ breeze/backend/maps/views.py
@@ -21,67 +21,82 @@ User = get_user_model()
 
 @api_view(['GET'])
 def get_middle(request):
-    subway = pd.read_csv('data/subway.csv')
-    print(subway.columns)
-    user1 = [37.52, 126.88]
-    user2 = [37.68, 127.06]
-    user3 = [37.53, 126.96]
+    passenger = pd.read_csv('data/passenger.csv')
+    participants = [[37.52, 126.88], [37.68, 127.06], [37.53, 126.96]]
 
-    # 중간 지점 찾기
-    # 연산 줄이기가 필요하다면 그라함 스캔 알고리즘
-    P = Polygon([user1, user2, user3])
+    # 중간 지점 찾기 (연산 줄이기가 필요하다면 그라함 스캔 알고리즘)
+    P = Polygon(participants)
     center = P.centroid
 
     # 위도 기준으로 차이가 적은 후보 역들 추리기
     diff = []
-    for lat in subway['latitude']:
+    for lat in passenger['latitude']:
         diff.append(abs(lat - center.x)) 
-    subway['diff'] = diff
-    subway_list = subway.sort_values('diff')[:20]
+    passenger['diff'] = diff
+    subway_list = passenger.sort_values('diff')[:20]
 
     # 후보 역들과 중간 지점의 거리 구하기
     dis = []
     for lat, long in zip(subway_list['latitude'], subway_list['longitude']):
-        print(lat, long)
         dis.append(np.sqrt((float(lat) - center.x)**2 + (float(long) - center.y)**2))
     subway_list['dis'] = dis
     final_subway = subway_list.sort_values('dis')[:10]
     final_subway = final_subway.drop(['diff'], axis=1)
+    final_subway = final_subway.drop(['id'], axis=1)
+    print(final_subway)
 
     name, latitude, longitude = '', '' ,''
     # 완벽한 중간: 중간 지점에서 거리차가 제일 적은 지하철 역
-    if request.middle_place_type == '완벽한 중간':
-        name = final_subway[0]['name']
-        latitude = final_subway[0]['latitude']
-        longitude = final_subway[0]['longitude']
+    mode1 = 0
+    # if request.mode1 == 0:
+    if mode1 == 0:
+        subway = final_subway.iloc[0]
+        name = subway['name']
+        latitude = subway['latitude']
+        longitude = subway['longitude']
+        print(subway)
     # 핫플레이스: 지하철 승하차승객수가 가장 많은 역
-    elif request.middle_place_type == '핫플레이스':
-        name = final_subway[0]
-        latitude = final_subway[0]
-        longitude = final_subway[0]
+    # elif request.mode1 == 1:
+    elif mode1 == 1:
+        subway = subway_list.sort_values('passenger', ascending=False).iloc[0]
+        name = subway['name']
+        latitude = subway['latitude']
+        longitude = subway['longitude']
     # 코로나멈춰: 지하철 승하차승객수가 가장 적은 역
     else:
-        name = final_subway[0]
-        latitude = final_subway[0]
-        longitude = final_subway[0]
+        subway = subway_list.sort_values('passenger').iloc[0]
+        name = subway['name']
+        latitude = subway['latitude']
+        longitude = subway['longitude']
 
-    # 중간 지점 찾고 사용자가 중간 지점으로 오는 경로 제공
-    headers = {
-        'Authorization': 'KakaoAK {REST_API_KEY}',
-    }
+    # 중간 지점 찾고 사용자가 중간 지점으로 오는 거리와 시간
+    time = []
+    for participant in participants:
+        participant_lat = participant[0]
+        participant_long = participant[1]
 
-    params = (
-        ('origin', '127.11015314141542, 37.39472714688412'),
-        ('destination', f'{longitude}, {latitude}'),
-        ('waypoints', ''),
-        ('priority', 'RECOMMEND'),
-        ('car_fuel', 'GASOLINE'),
-        ('car_hipass', 'false'),
-        ('alternatives', 'false'),
-        ('road_details', 'false'),
-    )
+        headers = {
+        'Authorization': f'KakaoAK {REST_API_KEY}',
+        }
 
-    response = requests.get('https://apis-navi.kakaomobility.com/v1/directions', headers=headers, params=params)
+        params = (
+            # ('origin', f'{participant_long},{participant_lat}'),
+            ('origin', '127.111202,37.394912'),
+            ('destination', f'{longitude},{latitude}'),
+            ('waypoints', ''),
+            ('priority', 'RECOMMEND'),
+            ('car_fuel', 'GASOLINE'),
+            ('car_hipass', 'false'),
+            ('alternatives', 'false'),
+            ('road_details', 'false'),
+        )
+
+        response = requests.get('https://apis-navi.kakaomobility.com/v1/directions', headers=headers, params=params).json()
+        # response.routes[0].section
+        time_min = response.get('routes')[0].get('summary').get('duration')
+        time_sec = round(time_min/60)
+        time.append(time_sec)
+    return Response(time)
 
 
 @api_view(['POST'])
