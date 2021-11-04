f18340a51f6f36807a8a2d93e173373b1e91c1e8
subin <ansubin2004@gmail.com>
2021-11-04 23:29:49 +0900

    ✨ feat: Update appointment note delete logic
    JIRA ISSUE: S05P31A202-133

--- breeze/backend/appointments/views.py
+++ breeze/backend/appointments/views.py
@@ -1,6 +1,9 @@
+from os import stat
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
 from .serializers import AppointmentSerializer
+from .models import Appointment
+from accounts.utils import check_login
 
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -8,15 +11,27 @@ from rest_framework.response import Response
 
 
 @api_view(['POST'])
+@check_login
 def appointment(request):
     pass
 
 
-@api_view(['GET', 'DELETE'])
-def appointment_note(request):
+@api_view(['GET'])
+def appointment_note(request, note_id):
     pass
 
 
+@api_view(['DELETE'])
+@check_login
+def appointment_mynote(request, note_id):
+    note = get_object_or_404(Appointment, id=note_id)
+    if request.user == note.user:
+        note.delete()
+        data = { 'access_token': request.access_token }
+        return Response(data, status=status.HTTP_204_NO_CONTENT)
+
+
 @api_view(['GET'])
+@check_login
 def appointment_list(request):
     pass
