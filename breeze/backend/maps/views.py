3d2fc608d79acc17b5b1f8a5e9149fa332bb31f8
김채은 <chenni0531@gmail.com>
2021-11-05 22:09:38 +0900

    🔨 fix: Update logic of finding midpoint

--- breeze/backend/maps/views.py
+++ breeze/backend/maps/views.py
@@ -22,8 +22,9 @@ User = get_user_model()
 @api_view(['GET'])
 def get_middle(request):
     passenger = pd.read_csv('data/passenger.csv')
-    participants = [[37.52, 126.88], [37.68, 127.06], [37.53, 126.96]]
-
+    participants = request.data.get('participants')
+    # participants = [[37.52, 126.88], [37.68, 127.06], [37.53, 126.96]]
+    
     # 중간 지점 찾기 (연산 줄이기가 필요하다면 그라함 스캔 알고리즘)
     P = Polygon(participants)
     center = P.centroid
@@ -45,10 +46,9 @@ def get_middle(request):
     final_subway = final_subway.drop(['id'], axis=1)
     print(final_subway)
 
+    mode1 = request.data.get('mode1')
     name, latitude, longitude = '', '' ,''
     # 완벽한 중간: 중간 지점에서 거리차가 제일 적은 지하철 역
-    mode1 = 0
-    # if request.mode1 == 0:
     if mode1 == 0:
         subway = final_subway.iloc[0]
         name = subway['name']
@@ -56,7 +56,6 @@ def get_middle(request):
         longitude = subway['longitude']
         print(subway)
     # 핫플레이스: 지하철 승하차승객수가 가장 많은 역
-    # elif request.mode1 == 1:
     elif mode1 == 1:
         subway = subway_list.sort_values('passenger', ascending=False).iloc[0]
         name = subway['name']
@@ -69,19 +68,19 @@ def get_middle(request):
         latitude = subway['latitude']
         longitude = subway['longitude']
 
+    
     # 중간 지점 찾고 사용자가 중간 지점으로 오는 거리와 시간
-    time = []
+    participant_info_list = []
     for participant in participants:
-        participant_lat = participant[0]
-        participant_long = participant[1]
+        participant_lat = participant.get('partLatitude ')
+        participant_long = participant.get('partLongitude ')
 
         headers = {
         'Authorization': f'KakaoAK {REST_API_KEY}',
         }
 
         params = (
-            # ('origin', f'{participant_long},{participant_lat}'),
-            ('origin', '127.111202,37.394912'),
+            ('origin', f'{participant_long},{participant_lat}'),
             ('destination', f'{longitude},{latitude}'),
             ('waypoints', ''),
             ('priority', 'RECOMMEND'),
@@ -92,11 +91,27 @@ def get_middle(request):
         )
 
         response = requests.get('https://apis-navi.kakaomobility.com/v1/directions', headers=headers, params=params).json()
-        # response.routes[0].section
         time_min = response.get('routes')[0].get('summary').get('duration')
         time_sec = round(time_min/60)
-        time.append(time_sec)
-    return Response(time)
+        participant_info = [{
+            'barami_type': participant.get('baramiType'),
+            'time': time_sec,
+        }]
+        participant_info_list.append(participant_info)
+
+    middle_data = [{
+        'middle_place_type': mode1,
+        'name': name,
+        'latitude': latitude,
+        'longitude': longitude,
+        'participants': participants,
+    }]
+
+    data = {
+        'access_token': request.access_token,
+        'middle_data': middle_data,
+    }
+    return Response(data, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
