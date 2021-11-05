7f2df3b5a1e3bc1fcfc8e8097f53da8b50ff71bc
DESKTOP-KVCQHCD\multicampus <jm9521@hanmail.net>
2021-11-05 18:57:23 +0900

    ✨ feat: Add FindPlaceBtn logic and store
    JIRA ISSUE: S05P31A202-124

--- breeze/frontend/src/components/findmiddle/FindPlaceBtn.vue
+++ breeze/frontend/src/components/findmiddle/FindPlaceBtn.vue
@@ -8,16 +8,71 @@
 </template>
 
 <script>
+import { mapState, mapGetters, mapActions } from 'vuex'
+
 export default {
   name: 'FindMiddleBtn',
   props: {
     middlePlaces: Array
   },
-  methods: {
-    goToFindPlace: function() {
-      this.$router.push({ name: 'FindPlace' })
+  data() {
+    return {
+      middleList: this.middlePlaces,
+      modeList: [],
+      partInfo: [],
     }
-  }
+  },
+  methods: {
+     ...mapActions([
+      'setPartMidTime',
+      'setMiddleName',
+      'setMiddleLat',
+      'setMiddleLong',
+    ]),
+    findMiddle(modeIdx, middleIdx) {
+      this.modeList = []
+      for (let i = 0; i < this.middleList.length; i++) {
+        if (this.middleList[i].middle_place_type === modeIdx) {
+          this.modeList.push(this.middleList[i])
+        }
+      }
+      return this.modeList[middleIdx]
+    },
+    concateArray(selectedMiddle) {
+      this.partInfo = []
+      for (let i = 0; i < selectedMiddle.participants.length; i++) {
+        for (let j = 0; j < this.participants.length; j++) {
+          if (selectedMiddle.participants[i].barami_type === this.participants[j].baramiType) {
+            this.partInfo.push({
+              "baramiType": this.participants[j].baramiType,
+              "partName": this.participants[j].partName,
+              "time": selectedMiddle.participants[i].time
+            })
+          }
+        }
+      }
+    },
+    async goToFindPlace() {
+      const middleInfo = await this.findMiddle(this.mode, this.middle)
+      await this.concateArray(middleInfo)
+      this.setMiddleName(middleInfo.name)
+      this.setMiddleLat(middleInfo.latitude)
+      this.setMiddleLong(middleInfo.longitude)
+      this.setPartMidTime(this.partInfo)
+      this.$router.push({ name: 'FindPlace' })
+    },
+
+  },
+  computed: {
+    ...mapState({
+    mode: state => state.mode.mode1,
+    middle: state => state.mode.middle,
+  }),
+  ...mapGetters([
+    'participants'
+  ])
+},
+
 }
 </script>
 
--- breeze/frontend/src/store/appointment/appointment.js
+++ breeze/frontend/src/store/appointment/appointment.js
@@ -3,10 +3,16 @@ export default {
     date: new Date(),
     timezone: 'Asia/Seoul',
     participants: [],
+    //약속쪽지에서 필요한 참여자 정보 리스트(타입, 이름, 시간)
+    partMiddleTime: [],
+    middleName: '',
+    middleLatitude: 0,
+    middleLongitude: 0
   },
   getters: {
     date: state => state.date,
     participants: state => state.participants,
+    partMiddleTime: state => state.partMiddleTime
   },
   mutations: {
     SETDATE (state, data) {
@@ -21,7 +27,19 @@ export default {
     //전체 참여자 초기화
     SETPARTICIPANTS (state) {
       state.participants = []
-    }
+    },
+    SETPARTMIDTIME (state, data) {
+      state.partMiddleTime = data
+    },
+    SETMIDDLENAME (state, data) {
+      state.middleName = data
+    },
+    SETMIDDLELAT (state, data) {
+      state.middleLatitude = data
+    },
+    SETMIDDLELONG (state, data) {
+      state.middleLongitude = data
+    },
   },
   actions: {
     setDate ({ commit }, data) {
@@ -35,6 +53,18 @@ export default {
     },
     setParticipants ({ commit }) {
       commit('SETPARTICIPANTS')
+    },
+    setPartMidTime ({ commit }, data) {
+      commit('SETPARTMIDTIME', data)
+    },
+    setMiddleName ({ commit }, data) {
+      commit('SETMIDDLENAME', data)
+    },
+    setMiddleLat ({ commit }, data) {
+      commit('SETMIDDLELAT', data)
+    },
+    setMiddleLong ({ commit }, data) {
+      commit('SETMIDDLELONG', data)
     },
   },
 }
