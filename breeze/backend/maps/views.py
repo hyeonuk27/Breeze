fbb2eb17ae0b056ad499b07dd2b540ed6aca8e28
hyeonuk <hyeonuk27@gmail.com>
2021-11-09 17:41:06 +0900

    🔨 fix: Fix get place code of BE

--- breeze/backend/maps/views.py
+++ breeze/backend/maps/views.py
@@ -32,27 +32,28 @@ def get_middle(request):
 @check_login
 def get_store(request, category_id, filter_id):
     # 중간역에 해당하는 가게들 선택, 그 중 해당 카테고리 선택
-    category_store = Place.objects.filter(middle_name=request.data['middlePlace'], category_num=category_id)
+    category_store = Place.objects.filter(middle_name=request.data['middlePlace'][:-1], category_num=category_id)
     
+    stores = []
     # 전체 결과가 10개 이하면 다 반환
     if len(category_store) < 11:
         stores = category_store
     else:
         if filter_id == 0:
             # 인기순
-            stores = category_store.order_by('review').desc()[:10]
+            stores = category_store.order_by('-review')[:10]
         elif filter_id == 1:
             # 평점순
-            stores = category_store.order_by('score').desc()[:10]
+            stores = category_store.order_by('-score')[:10]
         elif filter_id == 2:
             # 랜덤
             random.shuffle(category_store)
             stores = category_store[:10]
-    
     data = {
         'access_token': request.access_token,
         'stores': stores
     }
+
     return Response(data, status=status.HTTP_201_CREATED)
 
 
