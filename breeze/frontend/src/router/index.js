2f22daa7b00901dd6614288cb44ff34d8999e947
DESKTOP-KVCQHCD\multicampus <jm9521@hanmail.net>
2021-11-11 01:24:17 +0900

    🔨 fix: Delete file and fix router

--- breeze/frontend/src/router/index.js
+++ breeze/frontend/src/router/index.js
@@ -66,7 +66,7 @@ const routes = [
     // beforeEnter: checkToken()
   },
   {
-    path: '/makeappointment/:noteId',
+    path: '/makeappointment/:secretCode',
     name: 'MakeAppointment',
     component: MakeAppointment,
      // beforeEnter: checkToken()
