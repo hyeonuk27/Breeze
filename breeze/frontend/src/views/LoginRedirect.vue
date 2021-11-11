e55ec88c1ba55749d5734a9bad3f454937e8f694
김채은 <chenni0531@gmail.com>
2021-11-11 18:43:13 +0900

    ✨ feat: Add loading spinners
    JIRA ISSUE: S05P31A202-195

--- breeze/frontend/src/views/LoginRedirect.vue
+++ breeze/frontend/src/views/LoginRedirect.vue
@@ -1,5 +1,9 @@
 <template>
-  
+  <div class="spinner">
+    <div class="bounce bounce1"></div>
+    <div class="bounce bounce2"></div>
+    <div class="bounce bounce3"></div>
+  </div>
 </template>
 
 <script>
@@ -28,6 +32,46 @@ export default {
 </script>
 
 <style scoped>
+.spinner {
+  margin: 100px auto 0;
+  width: 100px;
+  text-align: center;
+}
 
+.spinner > div {
+  width: 18px;
+  height: 18px;
+  background-color: #B8D0FA;
+  border-radius: 100%;
+  display: inline-block;
+  -webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+  animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+}
 
+.spinner .bounce1 {
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+  margin-right: 10%;
+}
+
+.spinner .bounce2 {
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+  margin-right: 10%;
+}
+
+@-webkit-keyframes sk-bouncedelay {
+  0%, 80%, 100% { -webkit-transform: scale(0) }
+  40% { -webkit-transform: scale(1.0) }
+}
+
+@keyframes sk-bouncedelay {
+  0%, 80%, 100% { 
+    -webkit-transform: scale(0);
+    transform: scale(0);
+  } 40% { 
+    -webkit-transform: scale(1.0);
+    transform: scale(1.0);
+  }
+}
 </style>
