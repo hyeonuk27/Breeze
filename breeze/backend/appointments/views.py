d819a819382509adf47426b50e76478203d5f068
subin <ansubin2004@gmail.com>
2021-11-11 20:51:06 +0900

    🔨 fix: Update note random id

--- breeze/backend/appointments/views.py
+++ breeze/backend/appointments/views.py
@@ -10,14 +10,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 import datetime
-import crypto
-import sys
-sys.modules['Crypto'] = crypto
-
-import base64
-from Crypto import Random
-from Crypto.Cipher import AES
-
+import uuid
 
 User = get_user_model()
 
@@ -25,10 +18,12 @@ User = get_user_model()
 @check_login
 def appointment(request):
     user = get_object_or_404(User, id=request.user.id)
+    secret_code = str(uuid.uuid4())
     # 쪽지 저장
     note_data = {
         'datetime': request.data['dateTime'],
         'middle_place': request.data['middlePlace'],
+        'secret_code': secret_code,
     }
     sereializer = AppointmentSerializer(data=note_data, partial=True)
     if sereializer.is_valid(raise_exception=True):
@@ -58,11 +53,7 @@ def appointment(request):
         sereializer = ParticipantSerializer(data=paricipant_data, partial=True)
         if sereializer.is_valid(raise_exception=True):
             sereializer.save(appointment=appointment)
-
-    # node_id 암호화하여 보내기
-    secret_code = AESCipher(bytes(key)).encrypt(str(note_id))  
-
-        
+            
     data = { 
         'access_token': request.access_token,
         'note_id': note_id,
@@ -73,10 +64,9 @@ def appointment(request):
 
 @api_view(['GET'])
 def appointment_note(request, secret_code):
-    note_id = AESCipher(bytes(key)).decrypt(secret_code).decode('utf-8')
-    note = get_object_or_404(Appointment, id=note_id)
-    places = get_list_or_404(Appointmentplace, appointment_id=note_id)
-    participants = get_list_or_404(Participant, appointment_id=note_id)
+    note = get_object_or_404(Appointment, secret_code=secret_code)
+    places = get_list_or_404(Appointmentplace, appointment_id=note.id)
+    participants = get_list_or_404(Participant, appointment_id=note.id)
     
     place_data = []
     for place in places:
@@ -135,26 +125,3 @@ def appointment_list(request):
     }
 
     return Response(data, status=status.HTTP_200_OK)
-
-
-key = [0x10, 0x01, 0x15, 0x1B, 0xA1, 0x11, 0x57, 0x72, 0x6C, 0x21, 0x56, 0x57, 0x62, 0x16, 0x05, 0x3D,
-           0xFF, 0xFE, 0x11, 0x1B, 0x21, 0x31, 0x57, 0x72, 0x6B, 0x21, 0xA6, 0xA7, 0x6E, 0xE6, 0xE5, 0x3F]
-
-class AESCipher:
-    def __init__( self, key ):
-        self.key = key
-
-    def encrypt( self, raw ):
-        BS = 16
-        pad = lambda s: s + (BS - len(s.encode('utf-8')) % BS) * chr(BS - len(s.encode('utf-8')) % BS)
-        raw = pad(raw)
-        iv = Random.new().read( AES.block_size )
-        cipher = AES.new( self.key, AES.MODE_CBC, iv )
-        return base64.b64encode( iv + cipher.encrypt( raw.encode('utf-8') ) )
-
-    def decrypt( self, enc ):
-        unpad = lambda s : s[:-ord(s[len(s)-1:])]
-        enc = base64.b64decode(enc)
-        iv = enc[:16]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv )
-        return unpad(cipher.decrypt( enc[16:]))
