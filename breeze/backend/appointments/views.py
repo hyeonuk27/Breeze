4670c43e80cf6746afbb62698bc28194d1e7a172
subin <ansubin2004@gmail.com>
2021-11-11 17:46:51 +0900

    🔨 fix: Remove import crypto

--- breeze/backend/appointments/views.py
+++ breeze/backend/appointments/views.py
@@ -10,9 +10,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 import datetime
-import crypto
-import sys
-sys.modules['Crypto'] = crypto
+# import crypto
+# import sys
+# sys.modules['Crypto'] = crypto
 
 import base64
 from Crypto import Random
