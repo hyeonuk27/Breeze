6a58bd25e6e95a928b36247ff3176a2fa02d3384
DESKTOP-KVCQHCD\multicampus <jm9521@hanmail.net>
2021-11-14 11:16:45 +0900

    ✨ feat: Add login guard in refactor code

--- breeze/frontend/src/router/index.js
+++ breeze/frontend/src/router/index.js
@@ -10,28 +10,42 @@ import MakeAppointment from '@/views/MakeAppointment.vue'
 import OneClick from '@/views/OneClick.vue'
 import NotFound from '@/views/NotFound.vue'
 import authApi from '@/api/auth.js'
-import store from "@/store"
+import store from '@/store'
+import Swal from 'sweetalert2'
 
 
 Vue.use(VueRouter)
 
-const checkToken =  () => async (to, from, next) => {
-  if (from.name !== 'LoginRedirect') {
-    const userId = store.getters.getUserId
-    const isExpired = await authApi.check(userId)
-    if (isExpired) {
-      const response = await authApi.logout(userId)
-      if (response == 'success') {
-        await store.dispatch('removeUser')
-        sessionStorage.clear()
-        next('/')
+const checkLoginToken =  () => async (to, from, next) => {
+  const isLoggedIn = store.getters.getUserId
+  if (isLoggedIn) {
+    if (to.name == 'EnterInfo' || to.name == 'OneClick' || (to.name =='Home' && from.name !== 'LoginRedirect')) {
+      const isExpired = await authApi.check(isLoggedIn)
+      if(isExpired) {
+        const response = await authApi.logout(isLoggedIn)
+        if (response == 'success') {
+          await store.dispatch('removeUser')
+          sessionStorage.clear()
+          next('/')
+        }
+      } else {
+        return next()
       }
-    }
-    else {
+    } else {
       return next()
     }
+  } else {
+    Swal.fire({
+      html: '<b>로그인이 필요한 서비스입니다!</b>',
+      imageUrl: 'https://ifh.cc/g/bYAc4j.png',
+      imageWidth: 170,
+      imageHeight: 170,
+      imageAlt: 'Logo image',
+      showConfirmButton: false,
+      showCloseButton: true,
+    })
+    next('/')
   }
-  return next()
 }
 
 const routes = [
@@ -49,42 +63,42 @@ const routes = [
     path: '/home',
     name: 'Home',
     component: Home,
-    beforeEnter: checkToken()
+    beforeEnter: checkLoginToken()
   },
   {
     path: '/enterinfo',
     name: 'EnterInfo',
     component: EnterInfo,
-    beforeEnter: checkToken()
+    beforeEnter: checkLoginToken()
   },
   {
     path: '/findmiddle',
     name: 'FindMiddle',
     component: FindMiddle,
-    beforeEnter: checkToken()
+    beforeEnter: checkLoginToken()
   },
   {
     path: '/findplace',
     name: 'FindPlace',
     component: FindPlace,
-    beforeEnter: checkToken()
+    beforeEnter: checkLoginToken()
   },
   {
     path: '/makeappointment/:secretCode',
     name: 'MakeAppointment',
-    component: MakeAppointment,
-     beforeEnter: checkToken()
+    component: MakeAppointment
   },
   {
     path: '/oneclick',
     name: 'OneClick',
     component: OneClick,
-    beforeEnter: checkToken()
+    beforeEnter: checkLoginToken()
   },
   {
     path: '/404',
     name: 'NotFound',
-    component: NotFound
+    component: NotFound,
+    beforeEnter: checkLoginToken()
   },
   { path: '*',
     redirect: '/404'
